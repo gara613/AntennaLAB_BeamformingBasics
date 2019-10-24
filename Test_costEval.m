70e2ef121576adfd46ed5d7fe1cf5e21c1584a14
Germán A. Ramírez A <garamirezar@unal.edu.co>
2019-10-24 19:02:28 +0200

    Add files via upload

--- Test_costEval.m
+++ Test_costEval.m
@@ -6,7 +6,7 @@ inds = rand(50,12)>0.5; % (nInds x nSws) Logical or int matrix, each row is a sw
 inds = unique(inds,'rows');
 
 % Antenna Snp file, NOTE: It is mandatory that fields are saved in the same folder as the S-parameter matrix, using the same name scheme
-dataPath.SmatDir = 'D:\Users\german.ramirez\Documents\MATLAB\00 Sims and Meas\Reconfig PL\ReconfigPL_MediumSized_pixels\ReconfigPL.s13p';
+dataPath.SmatDir = 'D:\Users\german.ramirez\Documents\MATLAB\00 Sims and Meas\Reconfig PL\\ReconfigPL\ReconfigPL.s13p';
 % S2p On Sw parameters
 dataPath.SwsDir_On = 'D:\Users\german.ramirez\Documents\MATLAB\00 Sims and Meas\S_Parameters_Diodes\Manufacturer\MA4AGFCP910_10mA_Forward_Bias.s2p';
  % S2p On Sw parameters
@@ -15,12 +15,13 @@ dataPath.SwsDir_Off = 'D:\Users\german.ramirez\Documents\MATLAB\00 Sims and Meas
 options.Exhaustive = true;      %flag indicating if Exhaustive Search is to be performed 
 options.Combination = 'convex';	% (convex/none) Output creation (Non)Linear combination or MOGA
 options.idealSws = true;        % Debugging option to indicate optimization with idealSws
-options.connection = 'DiffPts'; % Indicates the way in which loads are connected to the antenna port
+options.connection = 'Conventional'; % Indicates the way in which loads are connected to the antenna port
+options.activePorts = 1;        %
 options.maxSval = 0;            % Maximum value for S parameters range
 options.targetS = -15;          % Optimization target S parameters
-options.maxIval = 5e-3;         % Maximum value for currents range
-options.targetI = 1e-1;         % Optimization target current
-options.targetTheta = 30;       % In degrees, Optimization target theta for pattern maximum
+options.maxIval = 1;            % Maximum value for currents range (relative to fed port maximum current)
+options.targetI = 0;            % Optimization target current
+options.targetTheta = 40;       % In degrees, Optimization target theta for pattern maximum
 options.targetPhi = 0;          % In degrees, Optimization target phi for pattern maximum
 options.maxAngDev = 10;         % Maximum value for angular deviation from target (In degrees)
 options.f_min = 3.45e9;         % Minimum frequency for S parameters range
@@ -34,23 +35,27 @@ a_cost = costEval_0x1(inds,dataPath,options);
 
 if options.Exhaustive
     options.Exhaustive = false; 
-    bestInd = dec2bin(pos(1)-1)-'0'; % Leftmost bit corresponds to the lowest index switch
-    [a, b] = costEval_0x1(dec2bin(pos(1:10)-1)-'0',dataPath,options);
+    bestInd = dec2bin(pos(1)-1,12)-'0'; % Leftmost bit corresponds to the lowest index switch
+    [a, b] = costEval_0x1(dec2bin(pos(1:10)-1,12)-'0',dataPath,options);
 else
     bestInd = inds(pos(1),:);
     [a, b] = costEval_0x1(inds(pos(1:10),:),dataPath,options);
 end
-
-bestInd
-[aa, bb, cc] = costEval_0x1(bestInd,dataPath,options);
-
 figure,
 plot(b.freqs,20*log10(abs(b.SL(1,:)))); % plotSpars
 title('Input port S-parameters of the best configuration')
 xlabel('frequency'); ylabel('dB'); grid on;
 
+bestInd
+[aa, bb, cc] = costEval_0x1(bestInd,dataPath,options);
+
 figure,
-imagesc(cc.Pattern);
+plot(bb.freqs,20*log10(abs(bb.SL))); % plotSpars
+title('Input port S-parameters of the best configuration')
+xlabel('frequency'); ylabel('dB'); grid on;
+
+figure,
+imagesc(squeeze(cc.Pattern(1,:,:)));
 title('Combined radiation pattern for the best configuration');
 xlabel('\phi'); ylabel('\theta');
 set(gca,'XTick',0:10:73); set(gca,'XTickLabel',cc.phi(1,1:10:end)); 
@@ -61,4 +66,3 @@ min(a)
 b
 aa
 bb
-cc.Gain
